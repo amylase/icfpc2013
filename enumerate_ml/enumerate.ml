1f2aef9833b9d38bac5250c79e2c5640a58f4b0b
Shunsuke Ohashi <sohashi@nii.ac.jp>
2013-08-09 17:17:24 +0900

    ocaml batteries gokansei

--- enumerate_ml/enumerate.ml
+++ enumerate_ml/enumerate.ml
@@ -1,4 +1,4 @@
-open Batteries
+open Batteries_uni
 open MyStd
 
 type id = string
--- enumerate_ml/myStd.ml
+++ enumerate_ml/myStd.ml
@@ -1,4 +1,4 @@
-open Batteries
+open Batteries_uni
 
 let first  f (x, y) = (f x,   y)
 let second f (x, y) = (  x, f y)
