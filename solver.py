3690106a507639603d802a6923b0c834ea858f1d
Shunsuke Ohashi <sohashi@nii.ac.jp>
2013-08-10 04:41:56 +0900

    solver.py: retry query when refused by too many requests

--- solver.py
+++ solver.py
@@ -4,6 +4,8 @@ import client
 import subprocess
 import time
 
+wait_time = 1
+
 def problem_string(problem):
     ret = ''
     ret += str(problem['size']) + ' '
@@ -34,6 +36,7 @@ def guess_result_string(guess_result):
     return ret
 
 def solve(problem, solver_command):
+    print 'solver: problem:', problem
     # run actual solver as subprocess
     worker = subprocess.Popen(solver_command, stdin = subprocess.PIPE, stdout = subprocess.PIPE)
 
@@ -45,44 +48,54 @@ def solve(problem, solver_command):
     while True:
         raw_query = worker.stdout.readline()
         query = parse_query(raw_query)
-        print 'solver: raw_query:', raw_query
-        print 'solver: query:', query
         if query['type'] == 'eval':
-            result = client.post_eval(problem, query['arguments'])
-            if result['status'] == 'error':
-                print 'solver: error occured in eval query.'
-                print result['message']
-                time.sleep(5)
-                break
+            while True:
+                print 'solver: query:', query
+                time.sleep(wait_time)
+                result = client.post_eval(problem, query['arguments'])
+                if result['status'] == 'error':
+                    if result['message'].find('Too many requests') >= 0:
+                        print 'solver: Query refused because of too many requests. Retry.'
+                    else:
+                        print 'solver: error occured in eval query.'
+                        print result['message']
+                        return
+                else:
+                    break
             worker.stdin.write(eval_result_string(result) + '\n')
             worker.stdin.flush()
         else:
-            result = client.post_guess(problem, query['program'])
-            if result['status'] == 'error':
-                print 'solver: error occured in guess query.'
-                print result['message']
-                time.sleep(5)
-                break
+            while True:
+                print 'solver: query:', query
+                time.sleep(wait_time)
+                result = client.post_guess(problem, query['program'])
+                if result['status'] == 'error':
+                    if result['message'].find('Too many requests') >= 0:
+                        print 'solver: Query refused because of too many requests. Retry.'
+                    else:
+                        print 'solver: error occured in guess query.'
+                        print result['message']
+                        return
+                else:
+                    break
             worker.stdin.write(guess_result_string(result) + '\n')
             worker.stdin.flush()
             if result['status'] == 'win':
                 print 'solver: We have solved the problem id', problem['id'], '!!'
-                time.sleep(5)
-                break
-        time.sleep(5)
+                return
+
 
 def solve_honban(condition, command):
     problems = client.post_myproblems(update = True)
+    time.sleep(wait_time)
     for problem in problems:
         if condition(problem) and not problem.get('solved', False) and problem.get('timeLeft', 300.) > 0.:
-            print problem
             solve(problem, command)
 
 def solve_honban_id(ids, command):
     problems = client.post_myproblems(update = False)
     for problem in problems:
         if problem['id'] == ids and not problem.get('solved', False) and problem.get('timeLeft', 300.) > 0.:
-            print problem
             solve(command)
 
 if __name__ == '__main__':
@@ -90,11 +103,20 @@ if __name__ == '__main__':
     command = sys.argv[1] if len(sys.argv) == 2 else './test_worker.py'
 
     print 'solver: start.'
+    for i in xrange(20):
+        while True:
+            train = client.post_train(size = 5)
+            if train.has_key('status'):
+                print train
+            else:
+                break
+        solve(train, command)
+
+    quit()
+
     def cond(prob):
         return prob['size'] <= 5
     solve_honban(cond, command)
 
-    # train = client.post_train(size = 13)
-    # solve(train, command)
 
 
